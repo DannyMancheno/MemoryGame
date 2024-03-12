cd822be47b0013637f85c9a9b959369cc6744fe9
Danny Mancheno <Danny.mancheno97@gmail.com>
2024-03-12 14:37:55 -0400

    Added PWA code - 1

---
+++ sw.js
@@ -0,0 +1,30 @@
+
+// https://github.com/DannyMancheno/MemoryGame
+
+
+// Repository Name
+var GHPATH = '/MemoryGame';
+
+// Choose a different pref (?)
+var APP_PREFIX = 'gppwa_'
+
+// App version
+var VERSION = '1.1.1'
+
+// Files needed for offline use
+var URLS = [
+    `${githubPath}/`,
+    `${githubPath}/index.html`,
+    `${githubPath}/MemoryGame.js`,
+    `${githubPath}/style.css`,
+    `${githubPath}/SoundFX/CardHide.mp3`,
+    `${githubPath}/SoundFX/CardMatch.mp3`,
+    `${githubPath}/SoundFX/CardReveal.mp3`,
+    `${githubPath}/SoundFX/CardWrong.mp3`,
+    `${githubPath}/SoundFX/TickTock.mp3`,
+    `${githubPath}/SoundFX/Victory.mp3`,
+    `${githubPath}/Images/Logo.png`,
+    `${githubPath}/MemoryGameLogo.png`,
+]
+
+
